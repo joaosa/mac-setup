950dff06721490dc9f74bd53df661f38463eaec1
Joao Andrade <jjadonotenter@gmail.com>
2018-07-09 14:36:12 +0100

    Setup a test install

---
+++ main.sh
@@ -0,0 +1,20 @@
+# xcode dev tools
+xcode-select --install
+
+# homebrew + brew cask
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# git
+brew install git-flow-avh
+brew install git-extras
+brew install hub
+
+# dotfiles
+git clone --bare https://github.com/joaosa/dotfiles ~/.cfg
+
+# zsh
+brew install zsh
+# prezto
+zsh
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+brew install fasd
