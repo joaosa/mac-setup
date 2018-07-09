b02cd84e1e601622dd62e9e3fb9d467fc384c6a4
Joao Andrade <jjadonotenter@gmail.com>
2018-07-09 15:48:18 +0100

    Copy the prezto config over and set zsh as the default shell

--- main.sh
+++ main.sh
@@ -16,5 +16,12 @@ git --git-dir=$HOME/.cfg/ --work-tree=$HOME reset --hard
 # zsh
 brew install zsh
 # prezto
-zsh -c "git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto""
+zsh << EOF
+git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
+setopt EXTENDED_GLOB
+for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
+  ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+done
+chsh -s /bin/zsh
+EOF
 brew install fasd
