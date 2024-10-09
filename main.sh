cbe4b0819a105e3c584a34dd47ae376cc7cd03b1
Joao Andrade <sousa.andrade.joao@gmail.com>
2024-10-09 16:09:21 +0100

    Escape heredoc var

--- main.sh
+++ main.sh
@@ -43,7 +43,7 @@ zsh << EOF
 git clone --recursive https://github.com/sorin-ionescu/prezto.git "${ZDOTDIR:-$HOME}/.zprezto"
 setopt EXTENDED_GLOB
 for rcfile in "${ZDOTDIR:-$HOME}"/.zprezto/runcoms/^README.md(.N); do
-  ln -sf "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+  ln -sf "\$rcfile" "${ZDOTDIR:-$HOME}/.\${rcfile:t}"
 done
 chsh -s /bin/zsh
 EOF
