cec16050b707b8053d9d3ce22ad9257e21fdc28c
João Sousa Andrade <659718+joaosa@users.noreply.github.com>
2024-10-09 20:42:13 +0100

    Simplify vim-plug installation

--- main.sh
+++ main.sh
@@ -56,7 +56,8 @@ stow -t "$HOME" "$(ls -d */)"
 cd - || exit
 
 # vim-plug
-sh -c 'curl -sfLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim && nvim -c ":PlugInstall | :qa"'
+curl -sfLo "$HOME/.local/share/nvim/site/autoload/plug.vim" --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+nvim -c ":PlugInstall | :qa"
 # tmux package manager
 tmux plugin manager
 TPM_PATH=~/.tmux/plugins/tpm
